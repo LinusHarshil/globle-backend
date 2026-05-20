d3a62feb7c4920bea3c02ef774ba630ca5067829
Linus Harshil <linusharshil7@gmail.com>
2026-05-20 15:03:14 +0530

    cookie error 3

--- source/routes/userRoutes.js
+++ source/routes/userRoutes.js
@@ -144,6 +144,8 @@ router.post("/login", async (req, res) => {
       httpOnly: true,
       secure: true,
       sameSite: "none",
+      domain: ".onrender.com",
+      path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -535,6 +537,8 @@ router.post("/logout", (req, res) => {
     httpOnly: true,
     secure: true,
     sameSite: "none",
+    domain: ".onrender.com",
+    path: "/",
   });
 
   return res.json({
